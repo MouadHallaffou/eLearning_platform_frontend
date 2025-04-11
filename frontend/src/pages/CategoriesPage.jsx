21b5b4e4be9fa819cc5c49fafe66dae75582f3b6
MouadHallaffou <mouadhallaffou@gmail.com>
2025-04-11 11:58:36 +0100

    feat: afficher la liste des catégories depuis l'API

---
+++ frontend/src/pages/CategoriesPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import CategoryList from '../components/categories/CategoryList'
+
+const CategoriesPage = () => {
+  return (
+    <>
+    <CategoryList/>
+    </>
+  )
+}
+
+export default CategoriesPage
